9ac7ae813ad36cbbe4cef9c3a2942a8a62639c07
Vinny Teixeira <87780451+vinnyteixeira@users.noreply.github.com>
2021-11-18 21:30:01 -0300

    Add files via upload

---
+++ src/RpgProject/Goblin.java
@@ -0,0 +1,106 @@
+package RpgProject;
+
+import java.util.Scanner;
+
+public class Goblin extends Personagem {
+	
+	Scanner ler = new Scanner(System.in);
+
+		private int forca;
+	
+		public Goblin(String nome) {
+			setNome(nome);
+			setVida(10);
+			setArmadura(7);
+			setForca(5);
+		}
+		
+		public Goblin(String nome, int xp, int vida, int armadura,int forca) {
+			setNome("Goblin "+nome);
+			setVida(vida);
+			setArmadura(armadura);
+			setForca(forca);
+		
+		}
+		
+		
+		public Goblin() {
+			setNome("Goblin");
+			setVida(15);
+			setArmadura(7);
+			setForca(5);
+		
+		}
+		
+		@Override
+		public int ataqueBasico() {
+			Dado dado= new Dado();
+			int valorAtaque=getForca()+dado.jogarDado(3);
+			System.out.println("\n"+getNome()+" deu um dano de "+valorAtaque+"!");
+			return valorAtaque;
+		}
+		@Override
+		public int ataqueFatal() {
+			Dado dado= new Dado();
+			int valorAtaque=getForca()+dado.jogarDado(3)+jogarDado(3)*jogarDado(3);
+			System.out.println("\n"+getNome()+" deu um dano de "+valorAtaque+"!");
+			return valorAtaque;
+		}
+		@Override
+		public int chamaAtaque() {
+			{
+				if (getBonusAtaque()>0) {
+					System.out.println("\nQual golpe você quer fazer?\n1-Apunhalada fraca\n2- Apunhalada forte");
+					int op = ler.nextInt();
+					switch(op) {
+					case 1: {
+						ataqueBasico();
+					}
+					case 2: {
+						ataqueFatal();
+						setBonusAtaque(getBonusAtaque()-1);
+					}
+					default: {
+						return 0;
+					}
+					}
+				}
+				else {
+					System.out.println("\nQual golpe você quer fazer?\n1- Apunhalada fraca)");
+					int op = ler.nextInt();
+					
+					switch(op) {
+					case 1: {
+						return ataqueBasico();
+					}
+					default:{
+						return 0;
+					}
+					}
+				}
+			}
+			
+		}
+		
+		
+		@Override
+		public void imprimirInfo(){
+			System.out.println("\n Personagem: "+ getNome()+"\n"+"Classe: "+ getClasse()+
+					"\nNível:"+getNivel()+"\nPontos de Vida: "+getVida()+"\nArmadura: "+getArmadura()+"\nPontos de Experiência: "+getXp()
+					+"\nForça: "+getForca()+"\nBônus de Golpe especial: "+getBonusAtaque());
+		}
+		
+		
+		public void setForca(int forca) {
+			this.forca=forca;
+		}
+		
+		public int getForca() {
+			return this.forca;
+		}
+		
+
+	
+	
+	
+}
--- src/RpgProject/TesteMenu.java
+++ src/RpgProject/TesteMenu.java
@@ -27,14 +27,20 @@ public class TesteMenu {
 					Guerreiro guerreiro = new Guerreiro(nome);
 					Orc orc = new Orc();
 					Lobisomen lobisomen = new Lobisomen();
-					System.out.println("\nVocê acaba de se deparar com o orc, que te acaba por te encurralar junto a parede.\nEle é bem maior que você, seus músculos enormes suados cheiram como um cadáver em estado de putrefação."
-							+ "\nEle segura um machado de duas mãos. O orc diz algumas palavras em sua língua nativa, \nporem você as interpreta como VOCÊ MORRE AQUI!\n");
+					Goblin goblin = new Goblin();
+					
 					Batalha batalha = new Batalha();
 					batalha.setOp1(guerreiro);
+					batalha.setOp2(goblin);
+					System.out.println("\nVocê passa por uma área com muitos arbustos e sente um forte odor, vindo de um dos arbustos de dentro de um deles sai um goblin que aparenta ser fraco porem hostil, ele te ataca sem que voce espere ");
+					batalha.comecarBatalha();
+					System.out.println("\n Parabens pela vitoria "+nome+"Mais sera que continuara sendo facil");
 					batalha.setOp2(orc);
+					System.out.println("\nVocê acaba de se deparar com o orc, que te acaba por te encurralar junto a parede.\nEle é bem maior que você, seus músculos enormes suados cheiram como um cadáver em estado de putrefação."
+							+ "\nEle segura um machado de duas mãos. O orc diz algumas palavras em sua língua nativa, \nporem você as interpreta como VOCÊ MORRE AQUI!\n");
 					batalha.comecarBatalha();
 					batalha.setOp2(lobisomen);
-					batalha.comecarBatalha();
+					System.out.println("\n");
 					
 					
 					
